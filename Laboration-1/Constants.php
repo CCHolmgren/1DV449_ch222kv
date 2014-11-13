3ae855855e7c3c64d1257bfd19eb0053400bcab3
Christfoffer Holmgren <ch222kv@student.lnu.se>
2014-11-13 15:27:24 +0100

    Added laboration 1 to the new project on github

---
+++ Laboration-1/Constants.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chrille
+ * Date: 2014-11-11
+ * Time: 13:50
+ */
+const TIME_BETWEEN_SCRAPES = 300;
+const EXECUTION_TIME_LIMIT = 100;
+const CURL_RETRY_LIMIT = 5;
+
+const USER_AGENT  = 'PHP cURL scraping Webbteknik II - Laboration 1 - ch222kv';
+
+const RESULT_DONEWHEN = "stoppedscrape";
+const NO_INFORMATION_TEXT = "no information";
+
+const RESULT_FILENAME = "result.json";
+const SCRAPING_STARTED_FILENAME = "started_scraping.txt";
+/*
+ * constant xpaths for the course page scraping
+ * Now I need the pagination scraping
+ * Maybe use an object later? So that I can create a lni kfrom the PLAN_XPATH without hardcoding it
+ */
+const COURSE_NAME_XPATH = '//*[@id="header-wrapper"]/h1/a/text()';
+const COURSE_URL_XPATH = '//*[@id="header-wrapper"]/h1/a/@href';
+const COURSE_CODE_XPATH = '//*[@id="header-wrapper"]/ul/li[3]/a/text()';
+//Kursplan also follows a nice format, but if we assume that it would follow that format, and they changed it
+//Then it would be bad. If we use this xpath to get to the kursplan then we can atleast assume we get it correct
+//As long as they do not rename the link or remove it
+const COURSE_PLAN_XPATH = "//a[text()='Kursplan']/@href";
+const COURSE_FIRST_XPATH = '//section/article[contains(@class, "start-page")]/div';
+const COURSE_HEADING_XPATH = "//article[2]/header/h1/a/text()";
+const COURSE_AUTHOR_XPATH = "//article[2]/header/p/strong/text()";
+const COURSE_PUBLISHED_XPATH = "//article[2]/header/p/text()[1]";
+
+const COURSE_NAME_NAME = "courseName";
+const COURSE_URL_NAME = "courseURL";
+const COURSE_CODE_NAME = "courseCode";
+const COURSE_PLAN_NAME = "coursePlan";
+const COURSE_FIRST_NAME = "courseFirst";
+const COURSE_HEADING_NAME = "courseHeading";
+const COURSE_AUTHOR_NAME = "courseAuthor";
+const COURSE_PUBLISHED_NAME = "coursePublished";
+
+$scrapeCoursePageArray = [COURSE_NAME_NAME      => COURSE_NAME_XPATH,
+                          //"courseURL"=>COURSE_URL_XPATH,
+                          COURSE_CODE_NAME      => COURSE_CODE_XPATH,
+                          COURSE_PLAN_NAME      => COURSE_PLAN_XPATH,
+                          COURSE_FIRST_NAME     => COURSE_FIRST_XPATH,
+                          COURSE_HEADING_NAME   => COURSE_HEADING_XPATH,
+                          COURSE_AUTHOR_NAME    => COURSE_AUTHOR_XPATH,
+                          COURSE_PUBLISHED_NAME => COURSE_PUBLISHED_XPATH
+];
+
+/*
+ * Constant xpaths for the course list scraping
+ */
+const PAGE_CURRENT_XPATH = "//div[@id='pag-top']/div/span[contains(@class, 'current')]/text()";
+const PAGE_NEXT_XPATH = "//div[@id='pag-top']/div/a[contains(@class, 'next')]/@href";
+const PAGE_ALL_COURSES_LINKS_XPATH = "//*[@id='blogs-list']/li/div[@class='item']/div[@class='item-title']/a/@href";
+const PAGE_ALL_COURSES_NAMES_XPATH = "//*[@id='blogs-list']/li/div[@class='item']/div[@class='item-title']/a/text()";
+
+const PAGE_CURRENT_NAME = "currentPath";
+const PAGE_NEXT_NAME = "nextPage";
+const PAGE_ALL_COURSES_LINKS_NAME = "courseLinks";
+const PAGE_ALL_COURSES_NAMES_NAME = "courseNames";
+
+$scrapeListPageArray = [PAGE_CURRENT_NAME => PAGE_CURRENT_XPATH, //Current page number
+                        PAGE_NEXT_NAME    => PAGE_NEXT_XPATH, //Next page path
+                        PAGE_ALL_COURSES_LINKS_NAME => PAGE_ALL_COURSES_LINKS_XPATH, //Links to courses
+                        //PAGE_ALL_COURSES_NAMES_NAME => PAGE_ALL_COURSES_NAMES_XPATH //Names of the courses
+
+];
+
---
+++ Laboration-1/index.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Chrille
+ * Date: 2014-11-05
+ * Time: 14:18
+ */
+
+require_once "Constants.php";
+require_once "Functions.php";
+require_once "ScrapeClass.php";
+require_once "CoursePage.php";
+require_once "CourseList.php";
+
+set_time_limit(EXECUTION_TIME_LIMIT);
+
+/*
+ * Methodology:
+ * Scrape the course list page, get all the courses
+ * Then we go through the list of the links and scrape those
+ * We can do this because doing it recursively doesn't help us. The courses are finite and as such we know its going to
+ * end sometime
+ * We create some form of object and then create a json object of those objects in an dictionary or something
+ */
+
+$timestarted = time();
+
+ob_end_clean();
+ob_start();
+header('Content-type: text/html; charset=utf-8');
+header("Connection: close");
+
+echo "
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset='UTF-8'>
+        <title>Scrapy scrapy</title>
+    </head>
+    <body>
+        <p>This page was generated at " . date("Y-m-d h:i:s", $timestarted) . "</p>";
+if (file_exists(SCRAPING_STARTED_FILENAME)) {
+    echo "<p>The scraping has already started. It might take a while, please return later to find out when it is loaded.</p>";
+    echo "<p>Started " . (time() - file_get_contents(SCRAPING_STARTED_FILENAME)) . " seconds ago.</p>";
+    ob_end_flush();
+    flush();
+    exit;
+} else if (file_exists("result.json")) {
+    $previous_result = get_result();
+    if ($previous_result[RESULT_DONEWHEN] + TIME_BETWEEN_SCRAPES > time()) {
+        $timenow = time();
+        echo "<p>Minimum time until next scrape: " . (TIME_BETWEEN_SCRAPES - ($timenow - $previous_result[RESULT_DONEWHEN])) . " seconds.</p>";
+        echo "<a href='result.json'>Result.json</a>";
+        //var_dump($previous_result);
+        ob_end_flush();
+        flush();
+        exit;
+    }
+}
+
+echo "<p>Starting the scraping of course list.</p>";
+echo "<a href='.'>Click here to see progress</a> or reload the page.";
+save_scraping_started_file($timestarted);
+//file_put_contents("started_scraping.txt", $timestarted);
+
+echo "
+    </body>
+</html>";
+
+//The browser will load until we tell it that the content has all been loaded, and that is done via this header setting
+$size = ob_get_length();
+header("Content-Length: $size");
+
+//Flush everything to the browser, and then end the streaming to the browser
+ob_end_flush();
+flush();
+session_write_close();
+
+//Initial things to start the scraping
+$start_path = "http://coursepress.lnu.se/kurser/";
+
+$courseList = new CourseList($start_path);
+$courseList->scrapeList();
+$courseList->scrapePages();
+
+save_result($courseList->getResults());
+//Who thought that unlink was a good name for removal of files?
+
+unlink(SCRAPING_STARTED_FILENAME);
