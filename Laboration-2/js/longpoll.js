a95bc8c2d66e0818034b83c880804ee90b632a95
Christfoffer Holmgren <ch222kv@student.lnu.se>
2014-11-24 15:16:01 +0100

    Starting to do the optimizations

---
+++ Laboration-2/js/longpoll.js
@@ -0,0 +1,3 @@
+/**
+ * Created by Chrille on 2014-11-24.
+ */
--- Laboration-2/messages.js
+++ Laboration-2/messages.js
@@ -1,3 +1,35 @@
+function Message(message, date) {
+
+    this.getText = function () {
+        return message;
+    }
+
+    this.setText = function (_text) {
+        message = text;
+    }
+
+    this.getDate = function () {
+        return date;
+    }
+
+    this.setDate = function (_date) {
+        date = date;
+    }
+
+}
+
+Message.prototype.toString = function () {
+    return this.getText() + " (" + this.getDate() + ")";
+}
+
+Message.prototype.getHTMLText = function () {
+
+    return this.getText().replace(/[\n\r]/g, "<br />");
+}
+
+Message.prototype.getDateText = function () {
+    return this.getDate().toLocaleTimeString();
+}
 var MessageBoard = {
 
     messages: [],
@@ -82,12 +114,12 @@ var MessageBoard = {
         }).done(function (data) {
             alert(data);
             $("#messagearea").prepend('<div class="message">' +
-                '<a href="#" class="clock">' +
-                     +
+            '<a href="#" class="clock">' +
+            +
                 '</a>' +
-                '<p>'+MessageBoard.nameField.value+' said:<br>'+MessageBoard.textField.value+'</p>' +
-                '<span>13:41:01</span>' +
-                '<span class="clear"></span>' +
+            '<p>'+MessageBoard.nameField.value+' said:<br>'+MessageBoard.textField.value+'</p>' +
+            '<span>13:41:01</span>' +
+            '<span class="clear"></span>' +
             '</div>');
             alert("Your message is saved! Reload the page for watching it");
         });
